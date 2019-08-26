2a192f5408176de6b0933d46a6822a66968990f2
Eric Ebulu <ekumamait@Eric-2.local>
2019-08-26 16:06:04 +0200

    ft(auth): implement User auth
    - Add user controllers
    - Add user models
    - Add .babelrc file
    - Add middleware for users
    
    [Delivers #167064107]

---
+++ server/database/db.js
@@ -0,0 +1,142 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.DB_URL
+  });
+  
+  pool.connect(() => {
+    console.log('connected to the db');
+  });
+
+export const query = (text, params) => {
+    return new Promise((resolve, reject) => {
+      pool.query(text, params)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      })
+    })
+  }
+  
+/**
+ * Create Orders Table
+ */
+export const createOrdersTable = () => {
+    const queryText =
+      `CREATE TABLE IF NOT EXISTS
+        orders(
+          order_id UUID PRIMARY KEY,
+          location TEXT NOT NULL,
+          quantity INT NOT NULL,
+          status TEXT NOT NULL,
+          user_id UUID NOT NULL,
+          order_date TIMESTAMP,
+          FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+        )`;
+  
+    pool.query(queryText)
+      .then((res) => {
+        console.log(res);
+        pool.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        pool.end();
+      });
+  }
+
+  /**
+ * Create Users Table
+ */
+export const createUsersTable = () => {
+    const queryText =
+      `CREATE TABLE IF NOT EXISTS
+        users(
+          user_id UUID PRIMARY KEY,
+          user_name TEXT NOT NULL,
+          user_email VARCHAR(128) UNIQUE NOT NULL,
+          user_password VARCHAR(128) NOT NULL,
+          admin BOOLEAN NOT NULL
+        )`;
+  
+    pool.query(queryText)
+      .then((res) => {
+        console.log(res);
+        pool.end();
+      })
+      .catch((err) => {
+        console.log(err);
+        pool.end();
+      });
+  }
+
+
+/**
+ * Drop Orders Table
+ */
+export const dropOrdersTable = () => {
+  const queryText = 'DROP TABLE IF EXISTS orders returning *';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+/**
+ * Drop Users Table
+ */
+export const dropUsersTable = () => {
+  const queryText = 'DROP TABLE IF EXISTS users returning *';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+/**
+ * Create All Tables
+ */
+export const createAllTables = () => {
+  createUserTable();
+  createOrdersTable();
+}
+
+/**
+ * Drop All Tables
+ */
+export const dropAllTables = () => {
+  dropUserTable();
+  dropOrdersTable();
+}
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+  module.exports = {
+    createOrdersTable,
+    createUsersTable,
+    createAllTables,
+    dropUsersTable,
+    dropOrdersTable,
+    dropAllTables,
+    query
+  };
+
+  require('make-runnable');
