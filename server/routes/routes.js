395afc094aef93ac950cde765476605ecdbd2a40
Eric Ebulu <ekumamait@Eric-2.local>
2019-08-30 12:36:43 +0200

    ft(menu): implement meals functionality
        - Add menu controllers
        - Delete models and rename database folder
    [Delivers #167353304]

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 // import auth from '../middlewares/auth';
 import Users from '../controllers/users';
 import Order from '../controllers/orders';
+import Menu from '../controllers/menu';
 
 
 // defining middleware routes
@@ -13,5 +14,11 @@ router.post('/api/auth/signin', Users.login);
 router.get('/api/users', Users.getAllUsers);
 router.delete('/api/users', Users.delete);
 router.post('/api/order', Order.create);
+router.get('/api/order', Order.getAll);
+router.get('/api/order', Order.getOne);
+router.post('/api/menu', Menu.create);
+router.get('/api/menu', Menu.getAll);
+router.get('/api/menu', Menu.getOne);
+router.delete('/api/menu', Menu.delete);
 
 export default router;
